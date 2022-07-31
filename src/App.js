35f6bacce7b3951be3696bac836ee89cfecbd303
PhantomOz <faniogor@gmail.com>
2022-07-31 18:24:33 +0100

    Currency Converter

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ExchangeTab from "./components/ExchangeTab";
+
+function App() {
+  return (
+    <div className="App">
+      <ExchangeTab />
+    </div>
+  );
+}
+
+export default App;
